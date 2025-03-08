16701bd1fbcefd2e29a6ef9b1f9d103069f269c8
Ilham <mi1797128@gmail.com>
2025-03-08 14:16:11 +0700

    feat: register upload image

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Models\Wallet;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Melihovv\Base64ImageDecoder\Base64ImageDecoder;
 
 class AuthController extends Controller
 {
@@ -32,8 +35,38 @@ class AuthController extends Controller
             return response()->json(['message' => 'Email already exists'],409);
         }
 
+        try {
+            $profilePicture = null;
+            $ktp = null;
+
+            if($request->profile_picture){
+                $profilePicture = $this->upload64Image($request->profile_picture);
+            }
+
+            if($request->ktp){
+                $ktp = $this->upload64Image($request->ktp);
+            }
+
+
+
+
+        } catch (\Throwable $th) {
+            echo $th;
+            return response()->json(['message' => 'Failed to upload image'],500);
+        }
+
 
         return response()->json(['message' => 'success'],200);
 
+    }
+    private function upload64Image($image64){
+        $decoder = new Base64ImageDecoder($image64, $allowedFormats = ['jpeg', 'png', 'gif', 'jpg']);
+        $deocedContent = $decoder->getDecodedContent();
+        $format = $decoder->getFormat();
+        $image = Str::random(10).'.'.$format;
+
+        Storage::disk('public')->put($image, $deocedContent);
+
+        return $image;
     }
 }
