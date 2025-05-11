ec3f4c836b8c5f24d6aaf7e8f78a8c7cbb207211
Ilham <mi1797128@gmail.com>
2025-05-11 10:35:46 +0700

    feat: menyimpan data transfer baik sender, receiver dan transfer history

--- app/Http/Controllers/Api/TransferController.php
+++ app/Http/Controllers/Api/TransferController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\PaymentMethod;
+use App\Models\Transaction;
+use App\Models\TransactionType;
+use App\Models\TransferHistory;
 use App\Models\User;
 use App\Models\Wallet;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
 
 class TransferController extends Controller
 {
@@ -67,10 +73,69 @@ class TransferController extends Controller
             ], 400);
         }
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Transfer successful'
-        ], 200);
-        
+        DB::beginTransaction();
+
+        try{
+
+            $trasactionType = TransactionType::whereIn('code', ['receive', 'transfer'])->orderBy('code', 'asc')->get();
+
+
+            $receiveTransactionType = $trasactionType->first();
+            $transferTransactionType = $trasactionType->last();
+
+            $transactionCode = strtoupper(Str::random(10));
+            $paymentMethod = PaymentMethod::where('code', 'bwa')->first();
+
+
+            // transaction for sender
+            $transferTransaction = Transaction::create([
+                'user_id' => $sender->id,
+                'transaction_type_id' => $transferTransactionType->id,
+                'description' => 'Transfer  to ' . $receiver->username,
+                'amount' => $data['amount'],
+                'transaction_code' => $transactionCode,
+                'status' => 'success',
+                'payment_method_id' => $paymentMethod->id,
+            ]);
+
+            // update balance sender
+            $senderWallet->decrement('balance', $data['amount']);
+
+            // create transaction for receiver
+            $transferTransaction = Transaction::create([
+                'user_id' => $sender->id,
+                'transaction_type_id' => $receiveTransactionType->id,
+                'description' => 'Receive from ' . $sender->username,
+                'amount' => $data['amount'],
+                'transaction_code' => $transactionCode,
+                'status' => 'success',
+                'payment_method_id' => $paymentMethod->id,
+            ]);
+            
+            Wallet::where('user_id', $receiver->id)->increment('balance', $data['amount']);
+
+            TransferHistory::create([
+                'sender_id' => $sender->id,
+                'receiver_id' => $receiver->id,
+                'transaction_code' => $transactionCode,
+            ]);
+
+            DB::commit();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Transfer success'
+            ], 200);
+
+
+            
+
+        }catch(\Exception $e){
+            DB::rollBack();
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Transfer failed'
+            ], 400);
+        }
     }
 }
